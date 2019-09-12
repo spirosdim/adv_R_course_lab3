640058de455ca015c07be9ef66341fe5effd6356
spirosdim <spirosdmtr@gmail.com>
2019-09-12 14:54:37 +0200

    add documentation examples

--- Rlab3/R/dijkstra.R
+++ Rlab3/R/dijkstra.R
@@ -1,5 +1,11 @@
 #' Dijkstra Algorithm
-#' 
-dijkstra <- function(){
-  
+#'
+#' @param A A data frame.
+#' @param n A number .
+#' @return 2
+#' @examples
+#' dijkstra(1,1)
+#' dijkstra(2,1)
+dijkstra <- function(A,n){
+  return(2)
 }
--- Rlab3/R/euclidean.R
+++ Rlab3/R/euclidean.R
@@ -1,5 +1,11 @@
 #' Euclidean division
 #'
-euclidean <- function(){
-  
+#' @param a A number.
+#' @param b A number.
+#' @return 2
+#' @examples
+#' euclidean(1,1)
+#' euclidean(2,1)
+euclidean <- function(a,b){
+  return(2)
 }
