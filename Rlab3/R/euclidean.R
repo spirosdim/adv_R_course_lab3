5670d74e13fa9312e452cb1597e8f0c4079227b3
spirosdim <spirosdmtr@gmail.com>
2019-09-14 19:06:24 +0200

    euclidean algorithm version 1.0

--- Rlab3/R/euclidean.R
+++ Rlab3/R/euclidean.R
@@ -2,10 +2,18 @@
 #'
 #' @param a A number.
 #' @param b A number.
-#' @return 2
+#' @return The Greatest Common Divisor of \code{a} and \code{b}.
 #' @examples
 #' euclidean(1,1)
 #' euclidean(2,1)
 euclidean <- function(a,b){
-  return(2)
+  stopifnot(exprs = {is.numeric(a); is.numeric(b); 
+    is.atomic(a); is.atomic(b); length(a)==1; length(b)==1})
+  
+  while (b!=0){
+    t <- b
+    b <- a%%b
+    a <- t
+  }
+  return(a)
 }
